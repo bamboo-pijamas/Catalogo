12f21e05c425952260efa475b12041292a23e3ca
Jose <josefe.cardenas@gmail.com>
2021-04-15 20:32:35 -0500

    imagenes comprimidas

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -2,86 +2,86 @@ $(document).ready(function () {
 
     $("div.01_rosa").click(function () {
         $(".modal-title").html("Camisa-Short");
-        $(".carrousel-1").attr("src","./Prendas/01_rosa_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/01_rosa.jpg")
-        $(".carrousel-3").attr("src","./Prendas/01_rosa_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/01_rosa_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/01_rosa-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/01_rosa_b-opt.jpg")
     })
 
     $("div.01_beige").click(function () {
         $(".modal-title").html("Camisa-Short");
-        $(".carrousel-1").attr("src","./Prendas/01_beige.jpg")
-        $(".carrousel-2").attr("src","./Prendas/01_beige.jpg")
-        $(".carrousel-3").attr("src","./Prendas/01_beige.jpg")
+        $(".carrousel-1").attr("src","./Prendas/01_beige-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/01_beige-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/01_beige-opt.jpg")
     })
 
     $("div.01_rosado").click(function () {
         $(".modal-title").html("Camisa-Short");
-        $(".carrousel-1").attr("src","./Prendas/01_rosado_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/01_rosado.jpg")
-        $(".carrousel-3").attr("src","./Prendas/01_rosado_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/01_rosado_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/01_rosado-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/01_rosado_b-opt.jpg")
     })
 
     $("div.02_rosa").click(function () {
         $(".modal-title").html("Camisa-Pantalón");
-        $(".carrousel-1").attr("src","./Prendas/02_rosa_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/02_rosa.jpg")
-        $(".carrousel-3").attr("src","./Prendas/02_rosa_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/02_rosa_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/02_rosa-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/02_rosa_b-opt.jpg")
     })
 
     $("div.02_beige").click(function () {
         $(".modal-title").html("Camisa-Pantalón");
-        $(".carrousel-1").attr("src","./Prendas/02_beige_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/02_beige.jpg")
-        $(".carrousel-3").attr("src","./Prendas/02_beige_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/02_beige_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/02_beige-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/02_beige_b-opt.jpg")
     })
 
     $("div.02_rosado").click(function () {
         $(".modal-title").html("Camisa-Pantalón");
-        $(".carrousel-1").attr("src","./Prendas/02_rosado.jpg")
-        $(".carrousel-2").attr("src","./Prendas/02_rosado.jpg")
-        $(".carrousel-3").attr("src","./Prendas/02_rosado.jpg")
+        $(".carrousel-1").attr("src","./Prendas/02_rosado-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/02_rosado-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/02_rosado-opt.jpg")
     })
 
     $("div.03_rosa").click(function () {
         $(".modal-title").html("Blusa-Short");
-        $(".carrousel-1").attr("src","./Prendas/03_rosa_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/03_rosa.jpg")
-        $(".carrousel-3").attr("src","./Prendas/03_rosa_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/03_rosa_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/03_rosa-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/03_rosa_b-opt.jpg")
     })
 
     $("div.03_beige").click(function () {
         $(".modal-title").html("Blusa-Short");
-        $(".carrousel-1").attr("src","./Prendas/03_beige_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/03_beige.jpg")
-        $(".carrousel-3").attr("src","./Prendas/03_beige_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/03_beige_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/03_beige-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/03_beige_b-opt.jpg")
     })
 
     $("div.03_rosado").click(function () {
         $(".modal-title").html("Blusa-Short");
-        $(".carrousel-1").attr("src","./Prendas/03_rosado_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/03_rosado.jpg")
-        $(".carrousel-3").attr("src","./Prendas/03_rosado_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/03_rosado_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/03_rosado-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/03_rosado_b-opt.jpg")
     })
 
     $("div.04_rosa").click(function () {
         $(".modal-title").html("Kimono-Pantalon");
-        $(".carrousel-1").attr("src","./Prendas/04_rosa_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/04_rosa.jpg")
-        $(".carrousel-3").attr("src","./Prendas/04_rosa_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/04_rosa_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/04_rosa-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/04_rosa_b-opt.jpg")
     })
 
     $("div.04_beige").click(function () {
         $(".modal-title").html("Kimono-Pantalon");
-        $(".carrousel-1").attr("src","./Prendas/04_beige.jpg")
-        $(".carrousel-2").attr("src","./Prendas/04_beige.jpg")
-        $(".carrousel-3").attr("src","./Prendas/04_beige_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/04_beige-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/04_beige-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/04_beige_b-opt.jpg")
     })
 
     $("div.04_rosado").click(function () {
         $(".modal-title").html("Kimono-Pantalon");
-        $(".carrousel-1").attr("src","./Prendas/04_rosado_a.jpg")
-        $(".carrousel-2").attr("src","./Prendas/04_rosado.jpg")
-        $(".carrousel-3").attr("src","./Prendas/04_rosado_b.jpg")
+        $(".carrousel-1").attr("src","./Prendas/04_rosado_a-opt.jpg")
+        $(".carrousel-2").attr("src","./Prendas/04_rosado-opt.jpg")
+        $(".carrousel-3").attr("src","./Prendas/04_rosado_b-opt.jpg")
     })
 
 
