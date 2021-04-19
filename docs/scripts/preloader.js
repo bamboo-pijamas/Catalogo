b525be4bb49792acae0e2542eb39465f16590869
Jose <josefe.cardenas@gmail.com>
2021-04-19 01:21:35 -0500

    se esta desarrollando la pagina con más animaciones, se logra un preloader adecuado sobre el resto del contenido

--- docs/scripts/preloader.js
+++ docs/scripts/preloader.js
@@ -1,4 +1,5 @@
 var preloader = document.getElementById('preloader');
 setTimeout(function() {
-  preloader.classList.add('cerrarPreloader');
+  preloader.classList.add('cerrarPreloader');;
+  document.body.style.overflowY= "visible";// despueés de cargar le devolvemos el scroll
 }, 6500);
